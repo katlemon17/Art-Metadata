abc0976461cd26b89a93fa19ec554435d1803971
katlemon17 <katherinetaylor317@gmail.com>
2021-01-17 15:27:37 -0500

    Added data pull and R project files

---
+++ code/01_data_pull.R
@@ -0,0 +1,10 @@
+# Katherine Taylor
+# 01_data_pull.R
+
+library(tidyverse)
+
+artwork <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-12/artwork.csv')
+artists <- readr::read_csv("https://github.com/tategallery/collection/raw/master/artist_data.csv")
+
+write_csv(artwork, here::here("data","raw_data","artwork.csv"))
+write_csv(artists, here::here("data","raw_data","artists.csv"))
